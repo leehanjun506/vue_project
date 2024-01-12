1404a37fb95848fa41a364d1689401f5f3190710
hanjun <sis03017@naver.com>
2024-01-12 19:04:21 +0900

    1/12

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="name">{{name}}</div>
+  <button
+      class="btn btn-primary"
+  v-on:click="updateName">Click</button>
 </template>
 
 <script>
+import {ref} from "vue";
+
 export default {
   setup() {
-    const name = 'Kossie Coder';
+    const name = ref('Han jun1');
+
+    // const greeting = (name) =>{
+    //   return 'Hello, ' + name;
+    // };
+    const updateName = () =>{
+      name.value = name.value === 'Han jun1' ? 'Han jun2' : 'Han jun1'
+      console.log(name);
+    }
 
     return {
-      name
+      name,
+      updateName
     };
   }
 }
